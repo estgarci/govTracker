ad36c2dcb96f76f11c3efb2e6c035dd1ca820c7f
esteban garcia <egarci@fastmail.com>
2022-09-10 18:28:34 -0700

    cleaning code

--- parse_candidates.py
+++ parse_candidates.py
@@ -44,7 +44,6 @@ for section in main_sections:
             position = section.find_element(By.TAG_NAME, "h4").text
         position_candidates = section.find_elements(By.TAG_NAME, "li")
         
-        candidates_information = []
         for candidate in position_candidates:
             candidate_dict = dict(
                 name = '',
@@ -58,7 +57,6 @@ for section in main_sections:
             information = candidate.text.split("<br></br>")[0].split('\n')
             information = list(filter(None, information))
             img_src = candidate.find_element(By.TAG_NAME, "img").get_attribute("src")
-
             information.append(img_src)
 
             candidate_dict["name"] = information[0]
@@ -66,12 +64,9 @@ for section in main_sections:
             candidate_dict["party"] = information[2]
             candidate_dict["funding"] = information[3]
             candidate_dict["image"] = information[4]
-
-            candidates_information.append(information)
             
             #opening candidate's modal
             open_modal_button = WebDriverWait(candidate, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a")))
-            
             open_modal_button.click()
 
             #parsing candidate's 'additional' information: contact information, biography, statement
@@ -89,25 +84,12 @@ for section in main_sections:
                 candidate_dict[field_name] = field_content
 
             candidates_list.append(candidate_dict)
-            #parsing biography and statements
-            # information.append([popup_information.text for popup_information in candidate_popup_information])
-            
-            # candidates_information.extend(information)
         
             #closing modal
             close_modal_button = popup_modal.find_element(By.CSS_SELECTOR, value="a")
             close_modal_button.click()
 
-            print("Estimated Progress: ",len(candidates_list)/190*100,'%')
-
-
-
+            print("Estimated Progress: ",len(candidates_list)/166*100,'%')
 
 candidates_df = pd.DataFrame(candidates_list)
-
-print(candidates_df)
-
 candidates_df.to_csv("candidates.csv")
-
-
-# print(candidates_information[0])
